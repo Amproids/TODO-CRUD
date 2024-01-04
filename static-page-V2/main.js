f67c3de781c8110327bc9e1e3e272647a8a8f6e5
Andrew Parry <parryandrew47@gmail.com>
2024-01-03 22:23:05 -0700

    changed text input box to clear, updated about file.

--- static-page-V2/main.js
+++ static-page-V2/main.js
@@ -6,6 +6,7 @@ addToDoButton.addEventListener("click", function(){
     var paragraph = document.createElement("p");
     paragraph.classList.add("paragraph-styling");
     paragraph.innerText = inputField.value;
+    inputField.value = "";
     paragraph.addEventListener("click", function(){
         paragraph.style.textDecoration = "line-through";
     })
